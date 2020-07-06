93961928ed01abbd4206b3b6bcc1a04d0bbde0e4
wanhar <wanhar@sehatq.com>
2020-07-06 16:39:02 +0700

    Initial Commit

---
+++ Game Catalogue/Game_CatalogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_CatalogueApp.swift
+//  Game Catalogue
+//
+//  Created by Wanhar on 06/07/20.
+//
+
+import SwiftUI
+
+@main
+struct Game_CatalogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

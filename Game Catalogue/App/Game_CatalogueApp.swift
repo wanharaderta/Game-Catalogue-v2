4c9973b1812b07326f45c634e36ccb3d8f619484
wanhar <wanhar@sehatq.com>
2020-07-21 21:41:49 +0700

    database

--- Game Catalogue/App/Game_CatalogueApp.swift
+++ Game Catalogue/App/Game_CatalogueApp.swift
@@ -6,12 +6,51 @@
 //
 
 import SwiftUI
+import CoreData
 
 @main
 struct Game_CatalogueApp: App {
+    
+    @Environment(\.scenePhase) private var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .onChange(of: scenePhase) { phase in
+            switch phase {
+            case .active:
+                print("active")
+            case .inactive:
+                print("inactive")
+            case .background:
+                print("background")
+                saveContext()
+            @unknown default:
+                fatalError("\(#function), fatal error in switch statement for .onChange modifier")
+            }
+        }
+    }
+    
+    var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Games")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
         }
     }
 }
---
+++ Game Catalogue/ViewModels/DetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  DetailViewModel.swift
+//  Game Catalogue
+//
+//  Created by Wanhar on 21/07/20.
+//
+
+import Foundation
+
+class DetailViewModel: ObservableObject {
+    
+}

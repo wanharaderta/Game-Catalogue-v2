24609819bb86023dc93413b69a17095dfeabce26
wanhar <wanhar@sehatq.com>
2020-07-24 17:36:21 +0700

    UserDefault

--- Game Catalogue/Utils/Constants.swift
+++ Game Catalogue/Utils/Constants.swift
@@ -13,3 +13,6 @@ let IMAGE_DEFAULT : String = "https://dapp.dblog.org/img/default.jpg"
 let GAME: Game = Game(id: 0, name: "Grand Theft Auto V", slug: "grand-theft-auto-v", rating: 0.0, background_image: "https://media.rawg.io/media/games/6d1/6d1e229ed8a593bf75578965b2c13f50.jpg", released: "2013-09-17", rating_top: 5)
 
 let DETAIL_GAME_RESPONSE : GameDetailResponse = GameDetailResponse(id: 0, name: "Grand Theft Auto V", description_raw: "Grand Theft Auto V", rating: 0.0, background_image: "https://media.rawg.io/media/games/6d1/6d1e229ed8a593bf75578965b2c13f50.jpg", released: "2013-09-17", website: "", rating_top: 0, genres: nil, tags: nil, developers: nil)
+
+let NAME = "name"
+let EMAIL = "email"
--- Game Catalogue/Views/Profile/ProfileView.swift
+++ Game Catalogue/Views/Profile/ProfileView.swift
@@ -9,39 +9,90 @@ import SwiftUI
 import URLImage
 
 struct ProfileView: View {
+    
+    @State var email = UserDefaults.standard.value(forKey: EMAIL) as? String ?? "Wanhardaengmaro@gmail.com"
+    @State var name = UserDefaults.standard.value(forKey: NAME) as? String ?? "Wanhar"
+    @State var isEditing = false
+    
     var body: some View {
         
         NavigationView {
-            VStack {
-                HStack {
-                    Spacer()
-                    URLImage(URL(string: PHOTO)!) { proxy in
-                        proxy.image
-                            .resizable()
-                            .renderingMode(.original)
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 150, height: 150)
-                            .clipped()
-                            .cornerRadius(150)
-                            .shadow(radius: 10)
+            ZStack {
+                VStack(spacing: 0) {
+                    HStack{
+                        Spacer()
+                        URLImage(URL(string: PHOTO)!) { proxy in
+                            proxy.image
+                                .resizable()
+                                .renderingMode(.original)
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 150, height: 150)
+                                .clipped()
+                                .cornerRadius(150)
+                                .shadow(radius: 10)
+                        }
+                        Spacer()
                     }
-                    Spacer()
+                    
+                    ZStack(alignment:.topLeading){
+                        VStack {
+                            VStack(alignment: .leading) {
+                                Text("Nama").font(.headline).fontWeight(.light).foregroundColor(Color.init(.label).opacity(0.75))
+                                HStack{
+                                    if (isEditing){
+                                        TextField("Enter Your Name", text: $name)
+                                            .disabled(false)
+                                    } else {
+                                        TextField("Enter Your Name", text: $name)
+                                            .disabled(true)
+                                    }
+                                }
+                                Divider()
+                            }.padding(.bottom, 15)
+                            
+                            VStack(alignment: .leading) {
+                                Text("Email").font(.headline).fontWeight(.light).foregroundColor(Color.init(.label).opacity(0.75))
+                                HStack{
+                                    if (isEditing){
+                                        TextField("Enter Your Email", text: $email)
+                                            .disabled(false)
+                                    } else {
+                                        TextField("Enter Your Email", text: $email)
+                                            .disabled(true)
+                                    }
+                                    
+                                }
+                                
+                                Divider()
+                            }.padding(.bottom, 15)
+                            
+                            Button(action: {
+                                if isEditing {
+                                    UserDefaults.standard.set(self.name, forKey: NAME)
+                                    UserDefaults.standard.set(self.email, forKey: EMAIL)
+                                } else {
+                                    self.name = UserDefaults.standard.value(forKey: NAME) as? String ?? "Wanhar"
+                                    self.email = UserDefaults.standard.value(forKey: EMAIL) as? String ?? "Wanhardaengmaro@gmail.com"
+                                }
+                                self.isEditing.toggle()
+                            }){
+                                if isEditing {
+                                    Text("Save").foregroundColor(.white).frame(width: UIScreen.main.bounds.width - 120).padding()
+                                } else {
+                                    Text("Edit").foregroundColor(.white).frame(width: UIScreen.main.bounds.width - 120).padding()
+                                }
+                            }.background(Color.blue)
+                            .clipShape(Capsule())
+                            .padding(.top, 45)
+                        }
+                        .padding(30)
+                        .background(CustomShape(corner: .allCorners, radii: 30).fill(Color.gray.opacity(0.1)))
+                    }.padding(15)
                 }
-                
-                Text("Wanhar")
-                    .bold()
-                    .font(.title)
-                    .padding()
-                Text("Wanhardaengmaro@gmail.com")
-                    .bold()
-                    .font(.subheadline)
-                
-                
-            }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+            }
             
             .navigationBarTitle("Profile")
-            .padding()
-        }
+        }.padding()
     }
 }
 
